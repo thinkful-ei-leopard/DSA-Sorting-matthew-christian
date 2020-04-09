3b6df4c6878dd31ba8519fc4e13ca5021f5fc189
shimmy77 <christianshim@gmail.com>
2020-04-09 14:22:43 -0500

    8 finished

--- drills.js
+++ drills.js
@@ -198,3 +198,48 @@ function random(array) {
 console.log(random(newArray))
 
 //8 sorting books
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+};
+
+function bubbleSort(array) {
+  let swaps = 0;
+  for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+          swap(array, i, i + 1);
+          swaps++;
+      }
+  }
+
+  if (swaps > 0) {
+      return bubbleSort(array);
+  }
+  return array;
+};
+
+const books = [
+  'A',
+  'B',
+  'D',
+  'C',
+  'F',
+  'E',
+  'J',
+  'G',
+  'I',
+  'H',
+  'K',
+  'L',
+  'N',
+  'M',
+  'O',
+  'P',
+  'R',
+  'Q',
+  'S',
+  'T'
+];
+
+console.log(bubbleSort(books))
